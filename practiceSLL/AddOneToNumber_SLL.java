a451d093f05f670aee078781d9baf6fb789cad1c
Ajinkya <ajinkya_awchat@yahoo.com>
2015-02-12 17:31:47 -0500

    Addition of 1 to a number[ represented by linked list] without reversing the linked list. e.g. 1->9->9->9  + 1 = 2->0->0->0

--- practiceSLL/AddOneToNumber_SLL.java
+++ practiceSLL/AddOneToNumber_SLL.java
@@ -41,6 +41,95 @@ public class AddOneToNumber_SLL {
 	}
 	
 	
+	
+	// Call to add
+	
+	public void addOneRecursiveCall(LL numLL, int num){
+		
+		// Create a new LL. For the result
+		LL newLL = new LL();
+		
+		Object[] receivedData = addOneRecursive(numLL.headNode, num);
+		
+		// Assign the node received as the headNode
+		newLL.headNode = (Node)receivedData[0];
+		
+		// Display the new added number
+		System.out.println();
+		newLL.displayLinkedList();
+		
+	}
+	
+	
+	//**********************************************
+	// Function to add 1 to the linked list without reversing
+	
+	public Object[] addOneRecursive(Node currNode, int num){
+		// This is the object that will be returned
+		Object[] returnData = new Object[2];
+		
+		if(currNode.next == null){
+			// Last digit. add one to it
+			int val = currNode.data + num;
+			if(val >= 10){
+				returnData[1] = 1;//(Integer)(1);
+				returnData[0] = new Node(val%10);
+			}
+			else{
+				returnData[1] = 0;//(Integer)(1);
+				returnData[0] = new Node(val);
+			}
+			return returnData;
+		}
+		else{
+			Object[] receivedData = addOneRecursive(currNode.next, num);
+			
+			// Unpack the objects received from the earlier call
+			int carry = (Integer)receivedData[1];
+			Node receivedNode = (Node)receivedData[0];
+			
+			//Create a new node and point it to the earlier node
+			Node newNode;
+			
+			if(carry == 1){
+				int val = currNode.data + 1;
+				if(val >= 10){
+					returnData[1] = 1;//(Integer)(1);
+					newNode = new Node(val%10);
+					// POint the current node to the receivedNode
+					newNode.next = receivedNode;
+					returnData[0] = newNode;
+				}
+				else{
+					returnData[1] = 0;//(Integer)(1);
+					newNode = new Node(val);
+					// POint the current node to the receivedNode
+					newNode.next = receivedNode;
+					returnData[0] = newNode;
+				}
+				
+				// Return the object array to the previous calls
+				return returnData; 
+			}
+			// If carry is zero, just append the current digits [as they would be added by 0]
+			else{
+				returnData[1] = 0;//(Integer)(1);
+				newNode = new Node(currNode.data);
+				// POint the current node to the receivedNode
+				newNode.next = receivedNode;
+				returnData[0] = newNode;
+			}
+			
+		}
+		return returnData;
+		
+	}
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		
 		AddOneToNumber_SLL addone = new AddOneToNumber_SLL();
@@ -51,9 +140,11 @@ public class AddOneToNumber_SLL {
 		newLL.insert(9);
 		newLL.insert(9);
 		newLL.insert(9);
+		newLL.insert(2);
 				
 		newLL.displayLinkedList();
 		
+		/*
 		// Reverse the linked list containing numbers
 		ReverseUsingTwoPointers reverse1 = new ReverseUsingTwoPointers();
 		newLL.headNode = reverse1.reverseLL(newLL.headNode, newLL.headNode.next);
@@ -65,6 +156,11 @@ public class AddOneToNumber_SLL {
 		
 		System.out.println();
 		addedNum.displayLinkedList();
+		*/
+		
+		// Adding number to 1 without reversing
+		addone.addOneRecursiveCall(newLL, 9);
+		
 	}
 
 }
