95c14524e98880bb6b59e01302eba3bc16231681
Ajinkya <ajinkya_awchat@yahoo.com>
2015-03-02 16:23:34 -0500

    Checking the symmetry of a binary tree, not BST

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -4,7 +4,7 @@ package Trees;
 
 import java.util.LinkedList;
 import java.util.Queue;
-
+import java.util.Stack;
 
 
 class BinaryTreeCreation {
@@ -63,8 +63,40 @@ class BinaryTreeCreation {
 				inorder(root.right);
 			}
 		}
+		
+		
+		
+		
+		// Function to check if the binary tree is symmetric
+		Stack stack = new Stack();
+		
+		public void checkSymmetry(){
+			// Push each node data into a stack during an in-order traversal
+			inorderPush(rootNode);
+			
+			// Pop and check each node data into a stack during an in-order traversal
+			inorderPop(rootNode);
+		}
+		
+		public void inorderPush(Node root){
+			if(root != null){
+				inorderPush(root.left);
+				stack.push(root.data);
+				inorderPush(root.right);
+			}
+		}
 	
-	
+		public void inorderPop(Node root){
+			if(root != null){
+				inorderPop(root.left);
+				if(root.data != (int)stack.pop()){
+					System.out.println("Not symmetric");
+					System.exit(0);
+				}
+				inorderPop(root.right);
+			}
+		}
+		
 }
 
 
@@ -81,6 +113,9 @@ public class BinaryTree{
 		binaryTree.insert(3);
 		
 		binaryTree.displayTree();
+		System.out.println();
+		binaryTree.checkSymmetry();
+		
 	}
 }
 
