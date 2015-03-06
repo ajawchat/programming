9603e9da580c435e9a0aa3e6bce4f73d8c3c58ef
Ajinkya <ajinkya_awchat@yahoo.com>
2015-03-05 21:27:21 -0500

    Added function to print pre-order traversal recursively

--- Trees/Tree1.java
+++ Trees/Tree1.java
@@ -1,5 +1,8 @@
 package Trees;
 
+import java.util.HashSet;
+import java.util.Stack;
+
 class Node{
 	int data;
 	Node left;
@@ -109,6 +112,120 @@ class BSTTree{
 	}
 	
 	
+	
+	// Get the maximum depth of a BST, works for any tree
+	public void maxDepth(){
+		int maxDepth = 0;
+		int currDepth = 0;
+		int newMaxDepth = traverseTree(root, currDepth); 
+		System.out.println("The max depth is "+newMaxDepth);
+	}
+
+	public int traverseTree(Node curr, int depth){
+		if(curr != null){
+			int left = traverseTree(curr.left, depth+1);
+			int right = traverseTree(curr.right, depth +1); 
+
+			if(left == 0 && right == 0)
+				return depth;
+			else if (left <= right)
+				return right;
+			else if (left > right)
+				return left; 
+		}
+		else{
+			return 0;
+		}
+		return 0;
+		
+	} 
+	
+	
+	//**********************************************************************
+	// Print the sum of all the nodes in the tree excluding the leaf nodes
+	public void getTotalSum(){
+		int sum = getSum(root);
+		System.out.println("Sum : "+sum);
+	}
+
+	private int getSum(Node curr){
+		if(curr.left == null && curr.right == null){
+			return 0;
+		}
+		else {
+			int left = 0;
+			if(curr.left != null) 
+				left = getSum(curr.left);
+			int right = 0;
+			if(curr.right != null) 
+				right = getSum(curr.right);
+			return (curr.data + left + right);
+		}
+		
+	}
+
+
+	//*****************************************************
+	// This function prints the stack in a pre-order manner iteratively. 
+	public void preorderIterative(Node root){
+		Stack stack = new Stack();
+		
+		HashSet<Integer> printed = new HashSet<Integer>();
+		
+		Node curr = root;
+		System.out.print(curr.data+"-");
+		
+
+		stack.push(curr);
+
+		while(!stack.isEmpty()){
+			while(curr.left != null){
+				if(curr.left != null){
+					curr = curr.left;
+					stack.push(curr);
+					System.out.print(curr.data+"-");
+					printed.add(curr.data);
+				}
+			}
+			
+			
+			curr = (Node)stack.pop();
+			while(curr.right == null){
+				//System.out.print(curr.data+"-");
+				if(curr.right == null && curr.left == null){
+					if(!printed.contains(curr.data)){
+						System.out.print(curr.data+"-");
+						printed.add(curr.data);
+					}
+						
+					break;
+				}
+				else{
+					System.out.println("in loop "+curr.data+"-");
+					curr = (Node)stack.pop();
+					System.out.println("after pop "+curr.data+"-");
+				}
+			}
+
+			// 	The current node has a right subtree, child
+			if(curr.right != null){
+				curr = curr.right;
+				if(!printed.contains(curr.data)){
+					System.out.print(curr.data+"-");
+					printed.add(curr.data);
+				}
+				stack.push(curr);
+			}
+	}
+	}
+	
+	
+	
+
+	
+	
+	
+	
 }
 
 
@@ -123,12 +240,21 @@ public class Tree1 {
 		newTree.insertNode(5);
 		newTree.insertNode(1);
 		newTree.insertNode(3);
-		//newTree.insertNode(6);
-		newTree.insertNode(2);
+		newTree.insertNode(6);
+		//newTree.insertNode(8);
+		//newTree.insertNode(2);
 		
 		newTree.displayTree();
 		
-		newTree.isBalanced();
+		//newTree.isBalanced();
+		
+		System.out.println();
+		//newTree.maxDepth();
+		
+		//newTree.getTotalSum();
+		newTree.preorderIterative(newTree.root);
+		
+		
 
 	}
 
