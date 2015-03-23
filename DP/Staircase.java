3ed372eccafc0360077c05dfae33bc835f1eaae6
Ajinkya <ajinkya_awchat@yahoo.com>
2015-03-23 19:14:05 -0400

    Dynamic programming approach to find the possible ways to climb stairs

--- DP/Staircase.java
+++ DP/Staircase.java
@@ -1,25 +1,64 @@
 package DP;
 
 public class Staircase {
-
+	
+	int[] counts;
+	
 	public int countWays(int n){
 		int ways = 0;
 		System.out.println("n="+n);
+		
+		if(n == 1){
+			counts[1] = 1;
+			return counts[1];
+		}
+		
 		for(int i=1 ; i<=3 ; i++){
-			if(n == i)
+			if(n == i){
 				ways += 1;
+				System.out.println("Adding one more way");
+			}
+				
 			else if(n > i){
 				System.out.println("n-i="+(n-i));
-				ways += countWays(n-i);
+				if(counts[n-i] > 0){
+					System.out.println("already stored value received for "+(n-i));
+					ways += counts[n-i];
+				}
+				else{
+					System.out.println("recalculating it");
+					counts[n-i] = countWays(n-i);
+					ways += counts[n-i];
+				}
 			}
 		}
+		counts[n] = ways;
 		System.out.println("For n="+n+", there are "+ways);
 		return ways;
 	}
 	
+	
+	public void initializeArray(int n){
+		counts = new int[n+1];
+		for(int i = 0; i < n+1; i++)
+			counts[i] = 0;
+	}
+	
+	public void printArray(){
+		for(int i = 0; i < counts.length; i++)
+			System.out.print(counts[i]+"-");
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		Staircase stairs = new Staircase();
-		System.out.println(stairs.countWays(2));
+		
+		int n = 36;
+		stairs.initializeArray(n);
+		
+		System.out.println(stairs.countWays(n));
+		stairs.printArray();
 	}
 
 }
